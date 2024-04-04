2a2d8679539df5dd1a74f48fea17bab8c6391f0a
Kim Minh Thang <thangved02@gmail.com>
2024-04-04 10:06:09 +0700

    feat: update template
    - Add comments
    - Add models

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -1,5 +1,12 @@
 import { AxiosInstance } from "axios";
 
+/**
+ * Base service class to be extended by other services
+ */
 export class BaseService {
+	/**
+	 * @description Constructor for the base service
+	 * @param http AxiosInstance to be used for making requests
+	 */
 	constructor(protected readonly http: AxiosInstance) {}
 }
--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,18 +1,45 @@
+/**
+ * @description Service for managing the access token
+ * - Get and set the access token
+ * - Save the access token to the local storage
+ * - Get the access token from the local storage
+ * - Remove the access token from the local storage
+ */
 export class TokenService {
+	/**
+	 * @description Constructor for the token service
+	 * @param _accessTokenName Name of the access token in the local storage
+	 * @param _accessToken Access token
+	 */
 	constructor(
 		private readonly _accessTokenName = "app:token:access",
 		private _accessToken: string = "",
 	) {
+		// Get the access token from the local storage if it exists
 		if (typeof window !== "undefined") this._accessToken = localStorage.getItem(this._accessTokenName) || "";
 	}
 
+	/**
+	 * @description Get the access token
+	 */
 	get accessToken() {
 		return this._accessToken;
 	}
 
+	/**
+	 * @description Set the access token
+	 */
 	set accessToken(token: string) {
 		this._accessToken = token;
 		if (typeof window !== "undefined") localStorage.setItem(this._accessTokenName, token);
+	}
+
+	/**
+	 * @description Clear the access token
+	 */
+	public clear() {
+		this._accessToken = ""; // Clear the access token
+		if (typeof window !== "undefined") localStorage.removeItem(this._accessTokenName); // Remove the access token from the local storage
 	}
 }
 
