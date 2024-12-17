de49d3203e3812d589f52213941785b5055b7303
Kim Minh Thang <thangved02@gmail.com>
2024-12-17 13:51:49 +0700

    chore(eslint): add eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,60 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { optimizeCssModules } from "vite-plugin-optimize-css-modules";
 import { VitePWA } from "vite-plugin-pwa";
 
+const plugins = [
+	react(),
+	nodePolyfills(),
+	optimizeCssModules(),
+	html({ minify: true }),
+	imagetools({
+		defaultDirectives() {
+			return new URLSearchParams({
+				format: "webp",
+			});
+		},
+	}),
+	VitePWA({
+		registerType: "autoUpdate",
+		selfDestroying: true,
+		manifest: {
+			name: "Vite React App",
+			short_name: "Vite React App",
+			description: "A Vite React app with PWA support",
+			background_color: "#ffffff",
+			theme_color: "#000000",
+			start_url: "/",
+			display_override: ["window-controls-overlay"],
+			icons: [
+				{
+					src: "pwa-64x64.png",
+					sizes: "64x64",
+					type: "image/png",
+				},
+				{
+					src: "pwa-192x192.png",
+					sizes: "192x192",
+					type: "image/png",
+				},
+				{
+					src: "pwa-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+				},
+				{
+					src: "maskable-icon-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+					purpose: "maskable",
+				},
+			],
+		},
+	}),
+	i18nextLoader({
+		namespaceResolution: "relativePath",
+		paths: ["./locales"],
+	}),
+];
+
 // https://vitejs.dev/config/
 export default () => {
 	return defineConfig({
@@ -18,59 +72,7 @@ export default () => {
 				},
 			},
 		},
-		plugins: [
-			react(),
-			nodePolyfills(),
-			optimizeCssModules(),
-			html({ minify: true }),
-			imagetools({
-				defaultDirectives() {
-					return new URLSearchParams({
-						format: "webp",
-					});
-				},
-			}),
-			VitePWA({
-				registerType: "autoUpdate",
-				selfDestroying: true,
-				manifest: {
-					name: "Vite React App",
-					short_name: "Vite React App",
-					description: "A Vite React app with PWA support",
-					background_color: "#ffffff",
-					theme_color: "#000000",
-					start_url: "/",
-					display_override: ["window-controls-overlay"],
-					icons: [
-						{
-							src: "pwa-64x64.png",
-							sizes: "64x64",
-							type: "image/png",
-						},
-						{
-							src: "pwa-192x192.png",
-							sizes: "192x192",
-							type: "image/png",
-						},
-						{
-							src: "pwa-512x512.png",
-							sizes: "512x512",
-							type: "image/png",
-						},
-						{
-							src: "maskable-icon-512x512.png",
-							sizes: "512x512",
-							type: "image/png",
-							purpose: "maskable",
-						},
-					],
-				},
-			}),
-			i18nextLoader({
-				namespaceResolution: "relativePath",
-				paths: ["./locales"],
-			}),
-		],
+		plugins,
 		resolve: {
 			alias: {
 				"@": fileURLToPath(new URL("./src", import.meta.url)),
