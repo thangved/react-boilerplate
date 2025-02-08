1a43cfce460ad8c84ec7ad258d238336976f6372
Kim Minh Thang <thangved02@gmail.com>
2025-02-08 23:07:14 +0700

    feat: update default logo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,47 @@
 import { fixupConfigRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/dist", "**/.eslintrc.cjs"],
-}, ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-)), {
-    plugins: {
-        "react-refresh": reactRefresh,
-    },
+export default [
+	{
+		ignores: ["**/dist", "**/.eslintrc.cjs"],
+	},
+	...fixupConfigRules(
+		compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended"),
+	),
+	{
+		plugins: {
+			"react-refresh": reactRefresh,
+		},
 
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
 
-        parser: tsParser,
-    },
+			parser: tsParser,
+		},
 
-    rules: {
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
-    },
-}];
+		rules: {
+			"react-refresh/only-export-components": [
+				"warn",
+				{
+					allowConstantExport: true,
+				},
+			],
+		},
+	},
+];
